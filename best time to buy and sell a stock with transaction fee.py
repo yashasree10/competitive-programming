d760efeaf8be88bce6a9cd4919e362e3659bfdb4
yashasree10 <163328988+yashasree10@users.noreply.github.com>
2024-03-16 10:58:02 +0530

    best time to buy and sell a stock with transaction fee.py

---
+++ best time to buy and sell a stock with transaction fee.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxProfit(self, prices, fee):
+        pos=-prices[0]
+        profit=0
+        n = len(prices)
+        for i in range(1,n):
+            pos = max(pos,profit-prices[i])
+            profit=max(profit,pos+prices[i]-fee)
+        return profit
