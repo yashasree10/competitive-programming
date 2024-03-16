f9a3b41cc7dc30bde0cf09509e66551db001eccf
yashasree10 <163328988+yashasree10@users.noreply.github.com>
2024-03-16 10:49:57 +0530

    Add files via upload

---
+++ baseball.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for i in operations:
+            match (i) :
+                case '+':
+                    op1 = stack[-1]
+                    op2 = stack [-2]
+                    stack.append(op1+op2)
+                case 'D':
+                    op = stack[-1]
+                    stack.append(2*op)
+                case 'C':
+                    stack.pop()
+                case _:
+                    stack.append(int(i))
+        sum = 0
+        if stack:
+            for i in stack:
+                sum = sum+i
+        return sum
