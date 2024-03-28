8ffe60222f6402c61d35d716e189c94faf8c5161
yashasree10 <163328988+yashasree10@users.noreply.github.com>
2024-03-28 10:45:51 +0530

    Create Decode String.py

---
+++ Decode String.py
@@ -0,0 +1,19 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack=[]
+        currStr=''
+        currNum=0
+        for c in s:
+            if c.isdigit():
+                currNum=currNum*10+int(c)
+            else:
+                if c=='[':
+                    stack.append((currStr,currNum))
+                    currStr=''
+                    currNum=0
+                elif c==']':
+                    prevStr,num=stack.pop()
+                    currStr=prevStr+num*currStr
+                else:
+                    currStr+=c
+        return currStr
